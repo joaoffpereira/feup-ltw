3f3ffc5b005f6e88bee733b2d6978a6273bcadbc
Henrique Ferrolho <henriqueferrolho@gmail.com>
2014-11-28 23:55:09 +0000

    modified footer

--- project/templates/footer.php
+++ project/templates/footer.php
@@ -2,15 +2,15 @@
 		<p>Copyright © 2014 PollHub</p>
 	</footer>
 
-	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-	<script src="assets/frameworks/bootstrap/js/bootstrap.min.js"></script>
-	<script src="actions/formDropdownButton.js"></script>
-
 	<!-- Bootstrap core JavaScript
 	================================================== -->
 	<!-- Placed at the end of the document so the pages load faster -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="assets/js/docs.min.js"></script>
+
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script src="assets/frameworks/bootstrap/js/bootstrap.min.js"></script>
+	<script src="actions/formDropdownButton.js"></script>
 
 	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 	<script src="assets/js/ie10-viewport-bug-workaround.js"></script>
