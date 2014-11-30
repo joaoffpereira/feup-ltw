74c8a458a330e986b7e3a8e3639fdf07081788ba
botas222 <pereiraffjoao1993@gmail.com>
2014-11-30 02:43:56 +0000

    isPrivate and anyoneCanAddOptions correctly saving on db

--- project/logic/classes/addPoll.php
+++ project/logic/classes/addPoll.php
@@ -3,14 +3,11 @@
 
 	try {
 
-		var_dump($_POST['isPublic']);
-		if(isset($_POST['isPublic']) && $_POST['isPublic'] == 'Yes') 
-			$isPublic = 1;
-		else $isPublic = 0;
+		(isset($_POST['isPrivate']) && $_POST['isPrivate'] === 'No') ? $isPrivate = 1 : $isPrivate = 0;
 
-		if(isset($_POST['anyoneCanAddOptions']) && $_POST['anyoneCanAddOptions'] == 'Yes') 
-			$anyoneCanAddOptions = 1;
-		else $anyoneCanAddOptions = 0;
+		(isset($_POST['anyoneCanAddOptions']) && $_POST['anyoneCanAddOptions'] === 'No') 
+			? $anyoneCanAddOptions = 1 : $anyoneCanAddOptions = 0;
+
 
 		$idCategory = $_POST["inputCategory"];
 		/*$image = $_POST["image"]; */
@@ -18,24 +15,24 @@
 		
 		$stmt = $dbh->prepare(
 			'INSERT INTO Poll
-			(idUser, isPublic, anyoneCanAddOptions, title, image, idCategory)
+			(idUser, isPrivate, anyoneCanAddOptions, title, image, idCategory)
 			VALUES (?,?,?,?,?,?)');
 		$stmt->execute(array(
 			$_SESSION['idUser'],
-			$isPublic,
+			$isPrivate,
 			$anyoneCanAddOptions,
 			$title,
 			$image,
 			$idCategory));
 
-	$idPoll = $dbh->lastInsertId();
-	
-	$questionN = 'question1';
+		$idPoll = $dbh->lastInsertId();
+		
+		$questionN = 'question1';
 
-	for($i=1; isset($_POST[$questionN]); $i++, $questionN = 'question'.$i) {
-		$question = $_POST[$questionN];
-		include("addQuestion.php");
-	}
+		for($i=1; isset($_POST[$questionN]); $i++, $questionN = 'question'.$i) {
+			$question = $_POST[$questionN];
+			include("addQuestion.php");
+		}
 
 	} catch(PDOException $e) {
 		echo $e->getMessage();
