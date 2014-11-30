ec01bf59c5d1e8a858651683de96b608b92f3760
arop <andre.r.o.pires@gmail.com>
2014-11-30 00:59:10 +0000

    removed useless button, working on view poll

--- project/templates/viewPoll.php
+++ project/templates/viewPoll.php
@@ -1,5 +1,5 @@
 <?php $pollId = $_GET['id']; 
-include('getPoll.php'); ?>
+include 'logic/classes/getPoll.php'; ?>
 
 <div class="modal fade" id="viewPollModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<div class="modal-dialog">
@@ -8,43 +8,27 @@ include('getPoll.php'); ?>
 				<button type="button" class="close" data-dismiss="modal">
 					<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
 				</button>
-				<h4 class="modal-title" id="myModalLabel">Poll</h4>
+				<h3 class="modal-title" id="myModalLabel"> <?= $poll['title']; ?> </h3>
 			</div>
 
-	  		<form id="questions" method="POST" action="actions.php?action=addPoll">
-				<div class="modal-body">
-					<div class="poll-details row vertical-align">
-
-						<div class="col-xs-6 col-sm-3">
-							<label><input type="checkbox" name="isPublic"> Private</label>
+			<div class="modal-body">
+				<?php foreach($poll['questions'] as $currentQuestion) { ?>
+				<div class="questions">
+					<div class="question" align="left">
+						<div class="question-text form-group-lg">
+						<p><?= var_dump($currentQuestion['question']); ?></p>
 						</div>
-
-						<div class="col-xs-6 col-sm-6">
-							<label><input type="checkbox" name="anyoneCanAddOptions"> Allow anyone to add options</label>
-						</div>
-					</div>
-
-					<?php foreach($poll['questions'] as $currentQuestion) ?>
-					<div class="questions">
-						<div class="question" align="left">
-							<div class="question-text form-group-lg">
-							<p><?= var_dump($currentQuestion['question']); ?></p>
-								<!--<input type="text" class="form-control input_question" size="100" placeholder=$question static>-->
-							</div>
-
 							<div class="question-option form-group-sm">
-								<!--<input type="text" class="form-control input_option" name="option1-1" size="100" placeholder="Add an option..."> <!--required="true"-->
-							</div>
 						</div>
 					</div>
-					<? endforeach(); ?>
 				</div>
+				<?php } ?>
+			</div>
 
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-					<button type="submit" class="btn btn-primary" name="submitcenas">Save changes</button>
-				</div>
-			</form>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+				<button type="submit" class="btn btn-primary" name="submitcenas">Save changes</button>
+			</div>
 		</div>
 	</div>
 </div>
