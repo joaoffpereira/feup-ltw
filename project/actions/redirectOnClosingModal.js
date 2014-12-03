5ee92a7440c2ebaca0c72b19d8230587b3cb6e59
arop <andre.r.o.pires@gmail.com>
2014-12-03 23:11:31 +0000

    added back button on poll results

--- project/actions/redirectOnClosingModal.js
+++ project/actions/redirectOnClosingModal.js
@@ -7,3 +7,11 @@ $('#viewPollModal').on('hide.bs.modal', function () {
 
   	window.location = 'index.php?page='.concat(newLocation);
 });
+
+function redirect(idPoll) {
+	if(document.getElementById('modalPreviousPage').innerHTML == " Feed")
+		newLocation = "feed";
+	else newLocation = "myPolls";
+
+	window.location = 'index.php?page=viewPoll&id='.concat(idPoll).concat("&previous=").concat(newLocation);
+}
