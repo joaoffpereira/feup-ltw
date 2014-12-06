9991ba1fdc85e640d09bf55d34b8a8eaddd8578c
botas222 <pereiraffjoao1993@gmail.com>
2014-12-06 00:20:29 +0000

    fixed title of view poll (aka previous)

--- project/templates/otherUserProfile.php
+++ project/templates/otherUserProfile.php
@@ -30,7 +30,7 @@
 						<?php foreach ($otherUser['polls'] as $currentPoll) { ?>
 						<div class="col-xs-12 col-sm-3 placeholder">
 							<div class="placeholder-container">
-								<a href=<?= "index.php?page=viewPoll&id=" . $currentPoll['idPoll'] . "&previous=Feed"; ?> id="modal-view">
+								<a href=<?= "index.php?page=viewPoll&id=" . $currentPoll['idPoll'] . "&previous=".urlencode($otherUser['username'] . "'s profile"); ?> id="modal-view">
 									<img class="resize-to-fit-and-center placeholder-containter-img" href=<?= "index.php?page=viewPoll&id=" . $currentPoll['idPoll'] . "&previous=Feed"; ?> id="modal-view" src="<?= $currentPoll['image'] != '' ? UPLOADS_URL . "/" . $currentPoll['image'] : 'assets/img/default-poll.png' ?>" alt="Default poll image">
 								</a>
 							</div>
